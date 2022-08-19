a6ada83cd4f248b9d4dc74df8e716c736ef48e96
Takashi Uesaka <takashiuesaka1224@gmail.com>
2022-08-19 11:52:55 +0900

    fix dotnet, throw error in GO

--- apis/trips/tripsgo/errorMessage_test.go
+++ apis/trips/tripsgo/errorMessage_test.go
@@ -26,3 +26,7 @@ func TestSerializeErrorReturnsJsonIncludesCustomMessageUnit(t *testing.T) {
 	//assert
 	assert.Equal(t, expected, actual)
 }
+
+func TestError(t *testing.T) {
+	assert.FailNow(t, "Throw error in test code for test.")
+}
